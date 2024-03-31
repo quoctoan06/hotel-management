8b8653539eaae9377d77dd3785e9fab573016de0
quoctoan <quoctoan06@gmail.com>
2024-03-31 23:23:50 +1100

    add: sanity studio, sanity schema, next sanity auth, auth page, dark/light theme, react toast

--- src/app/(web)/layout.tsx
+++ src/app/(web)/layout.tsx
@@ -3,6 +3,9 @@ import { Poppins } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
+import ThemeProvider from '@/components/ThemeProvider/ThemeProvider'
+import { NextAuthProvider } from '@/components/AuthProvider/AuthProvider'
+import Toast from '@/components/Toast/Toast'
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -24,11 +27,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <main className='font-normal'>
-          <Header />
-          {children}
-          <Footer />
-        </main>
+        <NextAuthProvider>
+          <ThemeProvider>
+            <Toast />
+            <main className='font-normal'>
+              <Header />
+              {children}
+              <Footer />
+            </main>
+          </ThemeProvider>
+        </NextAuthProvider>
       </body>
     </html>
   )
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,19 @@
+'use client';
+
 import Link from 'next/link';
-import {FaUserCircle} from 'react-icons/fa';
-import {MdDarkMode} from 'react-icons/md';
+import { useContext } from 'react';
+import { FaUserCircle } from 'react-icons/fa';
+import { MdDarkMode, MdOutlineLightMode } from 'react-icons/md';
+import { useSession } from 'next-auth/react';
+import Image from 'next/image';
+
+import ThemeContext from '@/context/themeContext';
 
 const Header = () => {
+    const {darkTheme, setDarkTheme} = useContext(ThemeContext);
+
+    const { data: session } = useSession();
+
     return (
         <header className="py-10 px-4 container mx-auto text-xl flex flex-wrap md:flex-nowrap items-center justify-between">
             <div className="flex items-center w-full md:2/3">
@@ -11,12 +22,46 @@ const Header = () => {
                 </Link>
                 <ul className='flex items-center ml-5'>
                     <li className='flex items-center'>
-                        <Link href='/auth'>
-                            <FaUserCircle className='cursor-pointer' />
-                        </Link>
+                        {session?.user ? (
+                            <Link href={`/users/${session.user.id}`}>
+                                {session.user.image ? (
+                                    <div className='w-10 h-10 rounded-full overflow-hidden'>
+                                        <Image
+                                            src={session.user.image}
+                                            alt={session.user.name!}
+                                            width={40}
+                                            height={40}
+                                            className='scale-animation img'
+                                        />
+                                    </div>
+                                ) : (
+                                    <FaUserCircle className='cursor-pointer' />
+                                )}
+                            </Link>
+                        ) : (
+                            <Link href='/auth'>
+                                <FaUserCircle className='cursor-pointer' />
+                            </Link>
+                        )}
                     </li>
                     <li className='ml-2'>
-                        <MdDarkMode className='cursor-pointer' />
+                        {darkTheme ? (
+                            <MdOutlineLightMode
+                                className='cursor-pointer'
+                                onClick={ () => {
+                                    setDarkTheme(false);
+                                    localStorage.removeItem('hotel-theme');
+                                }}
+                            />
+                        ) : (
+                            <MdDarkMode 
+                                className='cursor-pointer'
+                                onClick={ () => {
+                                    setDarkTheme(true);
+                                    localStorage.setItem('hotel-theme', 'true');
+                                }}
+                            />
+                        )}
                     </li>
                 </ul>
             </div>
